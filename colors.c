43e4ace259cfdf9a21d62e7b790fb4029a23c3b3
markgsaunders <36420212+markgsaunders@users.noreply.github.com>
2019-11-15 09:39:45 -0800

    Create colors.c

---
+++ colors.c
@@ -0,0 +1,11 @@
+#include "colors.h"
+
+void init_colors( void )
+{
+  return cy_rgb_led_init( CYBSP_LED_RGB_RED, CYBSP_LED_RGB_GREEN, CYBSP_LED_RGB_BLUE, CY_RGB_LED_ACTIVE_LOW );
+}
+
+void color( color_t color )
+{
+  cy_rgb_led_on( color, CY_RGB_LED_MAX_BRIGHTNESS );
+}
